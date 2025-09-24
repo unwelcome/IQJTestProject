2a7c27c34b5fcf7f85394e8f8483e3a362cd2984
unwelcome <moyapohtaxd@mail.ru>
2025-09-24 19:10:35 +0300

    created fiber project

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		fmt.Println("Request")
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":8080")
+}
